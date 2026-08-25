0b458a41a7b83db4af54c76956700e1b0103e049
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-08-25 17:00:53 +0300

    feat: add Client entity

---
+++ src/main/java/linx7a/entity/Client.java
@@ -0,0 +1,62 @@
+package linx7a.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+    @Column(name = "registration_date", nullable = false)
+    private LocalDate registrationDate;
+
+    public Client() {
+    }
+
+    public Client(String name, String email, LocalDate registrationDate) {
+        this.name = name;
+        this.email = email;
+        this.registrationDate = registrationDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", registrationDate=" + registrationDate +
+                '}';
+    }
+}

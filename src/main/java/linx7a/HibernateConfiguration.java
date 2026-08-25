e5e1fff2111801d4e5902194be9bbe07414b5e3c
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-08-25 16:41:22 +0300

    chore: add Hibernate configuration and verify Docker Postgres connection on port 5434

---
+++ src/main/java/linx7a/HibernateConfiguration.java
@@ -0,0 +1,22 @@
+package linx7a;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HibernateConfiguration {
+    @Bean
+        public SessionFactory sessionFactory() {
+            org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+            configuration
+                    .addPackage("linx7a")
+                    .setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
+                    .setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5434/postgres")
+                    .setProperty("hibernate.connection.username", "postgres")
+                    .setProperty("hibernate.connection.password", "root")
+                    .setProperty("hibernate.show_sql", "true")
+                    .setProperty("hibernate.hbm2ddl.auto", "update");
+            return configuration.buildSessionFactory();
+        }
+}

30642d19c56aac8fb378975580395220b21e4967
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-08-26 21:09:16 +0300

    feat: add id getter and setter to Client and Profile entities

--- src/main/java/linx7a/entity/Client.java
+++ src/main/java/linx7a/entity/Client.java
@@ -29,6 +29,14 @@ public class Client {
         this.registrationDate = registrationDate;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
--- src/main/java/linx7a/entity/Profile.java
+++ src/main/java/linx7a/entity/Profile.java
@@ -25,6 +25,14 @@ public class Profile {
         this.client = client;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getAddress() {
         return address;
     }

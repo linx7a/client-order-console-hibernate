200c21df172b1618c4889e8ee04501a31ef2a1cc
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-08-26 21:55:08 +0300

    feat: add Coupon entity

---
+++ src/main/java/linx7a/entity/Coupon.java
@@ -0,0 +1,70 @@
+package linx7a.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "coupons")
+public class Coupon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "code", nullable = false)
+    private String code;
+    @Column(name = "discount", nullable = false)
+    private float discount;
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+    public Coupon() {
+    }
+
+    public Coupon(String code, float discount, LocalDate expirationDate) {
+        this.code = code;
+        this.discount = discount;
+        this.expirationDate = expirationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Coupon{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", discount=" + discount +
+                ", expirationDate=" + expirationDate +
+                '}';
+    }
+}

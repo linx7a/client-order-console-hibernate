1f83a1b2f445e1f04d94049abe1926bf4eeb211a
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-08-26 21:45:37 +0300

    feat: add Order entity

---
+++ src/main/java/linx7a/entity/Order.java
@@ -0,0 +1,70 @@
+package linx7a.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "order_date", nullable = false)
+    private LocalDate orderDate;
+    @Column(name = "total_amount", nullable = false)
+    private Double totalAmount;
+    @Column(name = "status", nullable = false)
+    private String status;
+
+    public Order() {
+    }
+
+    public Order(LocalDate orderDate, Double totalAmount, String status) {
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", orderDate=" + orderDate +
+                ", totalAmount=" + totalAmount +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}

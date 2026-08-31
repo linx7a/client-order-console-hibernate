e4ba6dd51b32e445e5663a287e878a9060694989
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-08-31 11:25:48 +0300

    feat: add ConsoleOperationType enum for console menu

---
+++ src/main/java/linx7a/console/ConsoleOperationType.java
@@ -0,0 +1,11 @@
+package linx7a.console;
+
+public enum ConsoleOperationType {
+    USER_ADD,
+    USER_DELETE,
+    PROFILE_UPDATE,
+    ORDER_ADD,
+    COUPONS_UPDATE,
+    ORDER_FIND,
+    EXIT
+}

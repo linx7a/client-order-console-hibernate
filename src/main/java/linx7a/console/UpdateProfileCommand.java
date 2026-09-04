da258e16d5fc850dcb8dadb9e6d04668138fa523
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-09-04 19:18:09 +0300

    feat: add UpdateProfileCommand

---
+++ src/main/java/linx7a/console/UpdateProfileCommand.java
@@ -0,0 +1,47 @@
+package linx7a.console;
+
+import linx7a.entity.Client;
+import linx7a.entity.Profile;
+import linx7a.service.ClientService;
+import linx7a.service.ProfileService;
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+
+@Component
+public class UpdateProfileCommand implements OperationCommand {
+    private final ProfileService profileService;
+    private final ClientService clientService;
+
+    public UpdateProfileCommand(ProfileService profileService, ClientService clientService) {
+        this.profileService = profileService;
+        this.clientService = clientService;
+    }
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void execute() {
+        System.out.println("Выберите клиента по id:");
+        Long id = Long.parseLong(scanner.nextLine());
+        Client client = clientService.getById(id);
+        Profile profile = client.getProfile();
+
+        System.out.println("Новый адрес (Enter, чтобы не менять):");
+        String address = scanner.nextLine();
+        if (!address.isBlank()) {
+            profile.setAddress(address);
+        }
+        System.out.println("Новый номер телефона(Enter, чтобы не менять):");
+        String phone = scanner.nextLine();
+        if (!phone.isBlank()) {
+            profile.setPhone(phone);
+        }
+        profileService.updateProfile(profile);
+        System.out.println("Профиль успешно обновлён. Новый адрес клиента: " + profile.getAddress() + ",  номер телефона: " + profile.getPhone());
+
+    }
+    @Override
+    public ConsoleOperationType getOperationType() {
+        return ConsoleOperationType.PROFILE_UPDATE;
+    }
+}

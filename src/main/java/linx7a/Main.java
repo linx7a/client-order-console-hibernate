cbac860658e074838354790ce23f1b86752b0d0f
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-09-04 21:06:29 +0300

    test: manually verify UpdateProfileCommand via Main

--- src/main/java/linx7a/Main.java
+++ src/main/java/linx7a/Main.java
@@ -1,22 +1,14 @@
 package linx7a;
 
-import linx7a.console.AddClientCommand;
-import linx7a.entity.Client;
-import linx7a.entity.Order;
-import linx7a.service.ClientService;
-import linx7a.service.OrderService;
+import linx7a.console.UpdateProfileCommand;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext("linx7a");
+        UpdateProfileCommand updateProfileCommand = context.getBean(UpdateProfileCommand.class);
 
-        AddClientCommand addClientCommand = context.getBean(AddClientCommand.class);
-        addClientCommand.execute();
+        updateProfileCommand.execute();
     }
 }
